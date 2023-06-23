a1897c4ea5bf681afb57311fa1d4ea958aa176cf
Himanshu <himanshuparodiya@gmail.com>
2023-06-23 09:40:48 +0530

    PreLoader and fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Home from './components/HomeSection/Home'
 import Navbar from './components/NavBar/Navbar'
+import PreLoader from './components/PreLoader'
 
 function App() {
+  const [loader, setLoader] = useState(false)
+
+  useEffect(() => {
+    setLoader(true)
+    setTimeout(() => {
+      setLoader(false)
+    }, 5000);
+  
+ 
+  }, [])
+  
 
   return (
     <>
+      {loader 
+      ?
+        <PreLoader />
+        :
     <div className="app">
       <Navbar />
       <Home />
     </div>
+
+      }
     </>
   )
 }
---
+++ src/components/PreLoader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './PreLoader.css'
+import PreloaderVideo from '/src/assets/PreLoader/EasyFind.mp4'
+
+
+const PreLoader = () => {
+  return (
+    <div className="preloader" style={{width: "100vw"}}>
+      <video autoPlay muted>
+        <source src={PreloaderVideo} type="video/mp4" />
+      </video>
+      {/* Add any additional elements or styles for the preloader */}
+    </div>
+  )
+}
+
+export default PreLoader
