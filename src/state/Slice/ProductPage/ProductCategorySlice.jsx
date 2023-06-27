90ad58a8efeb04c5973702aac7d1517af8e33a49
Himanshu <himanshuparodiya@gmail.com>
2023-06-27 09:56:26 +0530

    working on store

---
+++ src/state/Slice/ProductPage/ProductCategorySlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const productCategorySlice = createSlice({
+    name: "productCategory",
+    initialState: '',
+    reducers:{
+        showAll(state,action){
+
+        }
+    }
+})
+
+export default productCategorySlice;
--- src/state/index.jsx
+++ src/state/index.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loadingSlice from "./Slice/loadingSlice";
+import productCategorySlice from "./Slice/ProductPage/ProductCategorySlice";
 
 const store = configureStore({
     reducer:{
         loading: loadingSlice.reducer,
+        productCategory: productCategorySlice.reducer,
     }
 })
 
